fbd6643a015902f0091c4c83e25d68233121961e
Brendan Lauck <brendanlauck@gmail.com>
2025-04-09 01:03:17 -0700

    enable cors in server

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	weekly_trends_handlers "scraper/pkg/rest/handlers/weekly_trends"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -15,6 +17,16 @@ func main() {
 func SetupRouter() *gin.Engine {
 	gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
+	router.Use(
+		cors.New(cors.Config{
+			AllowAllOrigins:  true,
+			AllowMethods:     []string{"GET", "OPTIONS"},
+			AllowHeaders:     []string{"Origin", "Content-Type"},
+			ExposeHeaders:    []string{"Content-Length"},
+			AllowCredentials: true,
+			MaxAge:           12 * time.Hour,
+		}),
+	)
 	weeklyTrendsHandler := weekly_trends_handlers.NewWeeklyTrendsHandler()
 
 	router.GET("/api/weekly_trends", weeklyTrendsHandler.Get)
